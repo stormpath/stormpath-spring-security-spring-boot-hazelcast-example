d5880832297eedd6d4c1a7bc63b56db81750d77b
Les Hazlewood <les@hazlewood.com>
2015-12-04 17:15:21 -0800

    cleaned up imports

--- src/main/java/com/stormpath/tutorial/config/CacheConfig.java
+++ src/main/java/com/stormpath/tutorial/config/CacheConfig.java
@@ -3,23 +3,14 @@ package com.stormpath.tutorial.config;
 import com.hazelcast.core.Hazelcast;
 import com.hazelcast.core.HazelcastInstance;
 import com.hazelcast.spring.cache.HazelcastCacheManager;
-import com.hazelcast.web.SessionListener;
-import com.hazelcast.web.spring.SpringAwareWebFilter;
 import com.stormpath.sdk.api.ApiKey;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
-import org.springframework.boot.context.embedded.FilterRegistrationBean;
-import org.springframework.boot.context.embedded.ServletListenerRegistrationBean;
 import org.springframework.cache.Cache;
 import org.springframework.cache.CacheManager;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.core.Ordered;
-import org.springframework.security.core.session.SessionRegistry;
-import org.springframework.security.core.session.SessionRegistryImpl;
 import org.springframework.security.web.csrf.CsrfTokenRepository;
-
-import javax.servlet.DispatcherType;
 
 @Configuration
 public class CacheConfig {
